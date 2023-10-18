4567beed78929016683751cb2c898a6f82d6248e
vanshika <vanshikabansal73@gmail.com>
2023-10-18 22:38:41 +0530

    crud

---
+++ controllers/university.js
@@ -0,0 +1,60 @@
+import university_modal from '../modals/university';
+
+
+async function getUniversities(req,res){
+    try {
+        let data = await university_modal.find();
+        res.send(data);
+      } catch (error) {
+        console.log("error:", error.message);
+        res.send(`eeror:${error.message}`);
+      }
+}
+
+async function getUnivercity(req,res){
+    try {
+        let { city } = req.body;
+        let data = await university_modal.findOne({city});
+        res.send(data);
+      } catch (error) {
+        console.log("error:", error.message);
+        res.send(`eeror:${error.message}`);
+      }
+}
+
+
+async function createUniversity(req,res){
+    try {
+      let data = await university_modal.create(req.body);
+        console.log("data inserted");
+        res.end();
+      } catch (error) {
+        console.log("error:", error.message);
+        res.send(`eeror:${error.message}`);
+      }
+}
+
+async function updateUniversity(req,res){
+    try {
+        let { _id } = req.params;
+        let data = await university_modal.updateOne({ _id }, { $set: req.body });
+        res.end();
+      } catch (error) {
+        console.log("error:", error.message);
+        res.send(`eeror:${error.message}`);
+      }
+}
+  
+
+async function deleteUniversity(req,res){
+    try {
+        let { _id } = req.params;
+        let data2 = await university_modal.deleteOne({ _id }, { $set: req.body });
+        res.end();
+      } catch (error) {
+        console.log("error:", error.message);
+        res.send(`eeror:${error.message}`);
+      }
+}
+  
+export {getUniversities,getUnivercity,createUniversity,updateUniversity,deleteUniversity};
