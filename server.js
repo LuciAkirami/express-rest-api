bf67ee26ef1f4f5a59c43fc4327b5b8bffdef54f
LuciAkirami <110820999+LuciAkirami@users.noreply.github.com>
2023-10-19 17:07:02 +0000

    formatted code wherever necessary

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 import express from "express";
-import connect_db from './utils/connect_db.js';
+import connect_db from "./utils/connect_db.js";
+import dotenv from "dotenv";
+
+// Load environment variables from .env
+dotenv.config();
+
+// Create an Express application
 const app = express();
-import data from './insertData.js';
+
+// Access environment variables
+const PORT = process.env.PORT || 5000;
+
+// Create a database connection
+connect_db();
+
+// Set up middleware for JSON parsing
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
-data();  //comment this out to resist insertion twice;
-connect_db();
-const port=5000;
-app.listen(port,()=>console.log(`server is running on port ${port}`));
+
+// Start the Express server
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
