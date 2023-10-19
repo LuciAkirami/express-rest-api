5d83e71dcc081f2b7b6013e888cf70d337903f3b
Vanshika <93903074+Vanshika-73@users.noreply.github.com>
2023-10-19 23:52:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ dotenv.config();
 
 // Create an Express application
 const app = express();
-<<<<<<< HEAD
 import data from './insertData.js';
 import university_routes from './routes/university.js'
-=======
 
 // Access environment variables
 const PORT = process.env.PORT || 5000;
@@ -19,19 +17,14 @@ const PORT = process.env.PORT || 5000;
 connect_db();
 
 // Set up middleware for JSON parsing
->>>>>>> 2814bf4c6d3f2386cd8df11a919a7833e54e16f0
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
-<<<<<<< HEAD
 app.use('/api',university_routes)
 connect_db();
 const port=5000;
 app.listen(port,()=>console.log(`server is running on port ${port}`));
-=======
-
 // Start the Express server
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
->>>>>>> 2814bf4c6d3f2386cd8df11a919a7833e54e16f0
