500d75ce5506ea05b746f8fb05d2d784273a0891
Vanshika <93903074+Vanshika-73@users.noreply.github.com>
2023-10-19 23:26:11 +0530

    Update connect_db.js

--- utils/connect_db.js
+++ utils/connect_db.js
@@ -9,7 +9,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 function connect_db() {
   mongoose
 <<<<<<< HEAD
-    .connect(`mongodb+srv://Vanshika:Vanshika@cluster0.hozpztk.mongodb.net/hacktoberfest?retryWrites=true&w=majority`)
+    .connect('`)
 =======
     .connect(MONGODB_URI)
 >>>>>>> 2814bf4c6d3f2386cd8df11a919a7833e54e16f0
