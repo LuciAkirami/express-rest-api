d273d5207cd0109d431970875c012407102688d7
Vanshika <93903074+Vanshika-73@users.noreply.github.com>
2023-10-20 07:58:13 +0530

    Update connect_db.js

--- utils/connect_db.js
+++ utils/connect_db.js
@@ -7,13 +7,7 @@ dotenv.config();
 const MONGODB_URI = process.env.MONGODB_URI;
 
 function connect_db() {
-  mongoose
-<<<<<<< HEAD
-    .connect('`)
-=======
-    .connect(MONGODB_URI)
->>>>>>> 2814bf4c6d3f2386cd8df11a919a7833e54e16f0
-    .then((res) => console.log("Mongodb is connected"));
+  mongoose.connect('`).connect(MONGODB_URI).then((res) => console.log("Mongodb is connected"));
 }
 
 export default connect_db;
